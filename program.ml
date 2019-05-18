c8bf0ff13bf0104cc0e42432475ccff344e6f868
AndraStana <andrushk_puff@yahoo.com>
2019-05-18 21:59:27 +0300

    Local changes started working on the graphhhhh

--- program.ml
+++ program.ml
@@ -74,8 +74,10 @@ let rec printStmt givenStmt = match givenStmt with
     | CompStmt (stmt1, stmt2) -> (printStmt stmt1); (printStmt stmt2)
     | AssignStmt(varName, exp) -> (printString varName); Printf.printf " = "; (printExp exp)
     | PrintStmt (exp) -> Printf.printf "Print "; (printExp exp)
-    | IfStmt (exp,stmt1,stmt2) -> Printf.printf " If( "; (printExp exp) ;  Printf.printf " ) { "; (printStmt stmt1);  Printf.printf "} else { "; (printStmt stmt1); Printf.printf " } "
-    | WhileStmt(exp, stmt) -> Printf.printf "While ( "; (printExp exp); Printf.printf " ) {"; (printStmt stmt); Printf.printf " } " ;;
+    (* | IfStmt (exp,stmt1,stmt2) -> Printf.printf " If( "; (printExp exp) ;  Printf.printf " ) { "; (printStmt stmt1);  Printf.printf "} else { "; (printStmt stmt1); Printf.printf " } "
+    | WhileStmt(exp, stmt) -> Printf.printf "While ( "; (printExp exp); Printf.printf " ) {"; (printStmt stmt); Printf.printf " } " ;; *)
+    | IfStmt (exp,stmt1,stmt2) -> Printf.printf " If( "; (printExp exp) ;  Printf.printf " ) "
+    | WhileStmt(exp, stmt) -> Printf.printf "While ( "; (printExp exp); Printf.printf " ) ";;
  
 let rec printSuccessors successors = match successors with 
     | h :: t -> Printf.printf "%d " h; (printSuccessors t)
@@ -86,28 +88,122 @@ let printNode givenNode = match givenNode with
          Printf.printf "\n Statement Number: "; (printInt numberStmt);
          Printf.printf "\n Successors: "; (printSuccessors listSuccessors);;
 
+let rec printControlFlowGraph graph = match graph with 
+    | h :: t ->  Printf.printf " \n \t \t Node: ";  printNode h; printControlFlowGraph t;
+    | [] ->  Printf.printf "\n-----------------------------";;
 
-Printf.printf "\n ************************************************************\n";;
-printNode ( AssignStmt ("a", Const 2), 1, [2;3;4] );;
-
-Printf.printf "\n ************************************************************\n";;
-printNode ( a, 1, [2;3;4] )  ;;
-
+ 
 (* -----------------------------------Control Flow Graph-------------------------------------- *)
 
 
-let createControlFlowGraph program = match program with
-        | CompStmt ( stmt1, stmt2 ) -> "CompStmt"
-        (* | AssignStmt ( varName, exp) -> "AssignStmt" intra fara noduri succesoare *)
-        | AssignStmt ( varName, exp) -> "AssignStmt" 
+
+
+       
+        (* | _ ->  [];; *)
+
+
+
+
+        (* | AssignStmt (varName, exp) -> let newList  = ( ( AssignStmt (varName, exp), 1, [] ) :: nodeList)  in  (let newList2 =  ( AssignStmt (varName, exp), 1, [] ):: newList in newList2) *)
+       
+       
+        (* | AssignStmt (varName, exp) -> let newList  = ( ( AssignStmt (varName, exp), 1, [] ) :: nodeList)  in  (let newList2 =  ( AssignStmt (varName, exp), 1, [] ):: newList in newList2) *)
+        (* | CompStmt ( stmt1, stmt2 ) -> ( stmt1, 1 , [] ):: nodeList; ( stmt2, 1 , [] ):: nodeList; nodeList *)
+
+        (* | AssignStmt (varName, exp) -> ( AssignStmt (varName, exp), 1, [] ):: nodeList; nodeList
+        | CompStmt ( stmt1, stmt2 ) -> ( stmt1, 1 , [] ):: nodeList; ( stmt2, 1 , [] ):: nodeList; nodeList *)
+
+        (* | CompStmt ( stmt1, stmt2 ) -> ( stmt1, nodeNumber , [nodeNumber + 1] ):: nodeList; ( stmt2, nodeNumber +1 , [] ):: nodeList; nodeList *)
+
         
-        | _ -> "DEFAULT";;
+
+
+let rec numberStatements program number =  match program with
+    | PrintStmt (exp) -> number + 1;
+    | AssignStmt (varName, exp) -> number + 1;
+    | CompStmt (stmt1, stmt2) -> number + (numberStatements stmt1 0) + (numberStatements stmt2 0)
+    | IfStmt (exp, stmt1, stmt2) -> number + (numberStatements stmt1 0) + (numberStatements stmt2 0)
+    | WhileStmt(exp, stmt) -> number + (numberStatements stmt 0);;
+
+let rec generateListOfIds number = match number with
+    | 0 -> []
+    | _ -> number::(generateListOfIds (number - 1) )
+
+
+        (* if number = 0 then (generateListOfIds (number - 1) number::list ) else list *)
+
+
+
+
+
+
+
+
+ let rec createList program nodeList nodeNumber nodeNumberOfLoop = match program with 
+       
+        | PrintStmt (exp) -> let newList1  = ( ( PrintStmt ( exp), nodeNumber, [] ) :: nodeList) in newList1 
+        | AssignStmt (varName, exp) -> let newList1  = ( ( AssignStmt (varName, exp), nodeNumber, [] ) :: nodeList)  in newList1
+        
+        
+        
+        (* ??????????? *)
+        (* | IfStmt (exp,stmt1,stmt2) -> let newList1 = ( ( IfStmt (exp,stmt1,stmt2), nodeNumber, [nodeNumber + 1; nodeNumber + 2] ) :: nodeList)  in
+                                        (
+                                            let newList2 =  ( stmt1, nodeNumber + 1, [] ) :: newList1  in
+                                            (
+                                                let newList3 =  ( stmt2, nodeNumber + 2, [] ) :: newList2 in
+                                                    newList3
+                                            )
+                                        ) *)
+
+
+
+
+        (* | CompStmt ( stmt1, stmt2 ) -> let newList1  =  (stmt1, nodeNumber, [nodeNumber + 1] ) :: nodeList  in (let newList2 = (stmt2, nodeNumber + 1, [] ) :: newList1 in newList2) *)
+        
+        (* | CompStmt ( stmt1, stmt2 ) -> match stmt1, stmt2 with
+         let newList1  =  (stmt1, nodeNumber, [nodeNumber + 1] ) :: nodeList  in (let newList2 = (stmt2, nodeNumber + 1, [] ) :: newList1 in newList2)
+         *)
+
+         (* | WhileStmt (exp, stmt) -> let newList =  (WhileStmt (exp, stmt), nodeNumber, []) :: nodeList in newList :: (createList, newList, nodeNumber + 1,nodeNumber) *)
+        
+        
+        
+        
+        
+        | _ -> [] ;; 
+    
+let createControlFlowGraph program = List.rev (createList program [] 1 0);;
+    
+
+
+(* 
+let rec getLeastMaximumType program className1 className2 =
+    let superClassesList1  = (getOrderedSuperClasses program className1)
+        and superClassesList2  = (getOrderedSuperClasses program className2)
+    in (getLeastMaximumTypeFromLists superClassesList1 superClassesList2);; *)
+
 
 (* -----------------------------------Samples-------------------------------------- *)
 
+(* printNode ( AssignStmt ("a", Const 2), 1, [2;3;4] );; *)
+
+(* Printf.printf "\n ************************************************************\n";;
+printNode ( a, 1, [2;3;4] )  ;; *)
 
 Printf.printf "\n ************************************************************ \n";;
-Printf.printf  " %s  \n" (createControlFlowGraph a);;
+(* Printf.printf  " %s  \n" (createControlFlowGraph a);; *)
+
+(* 
+let test =  AssignStmt("b", Const 2);; 
+printControlFlowGraph (createControlFlowGraph test);; *)
+
+
+let test2 =  CompStmt( AssignStmt("a", Const 1), AssignStmt("b", Const 2) );; 
+printControlFlowGraph (createControlFlowGraph test2);;
+
+(* printSuccessors (generateListOfIds 10) *)
+ 
 
 
 
